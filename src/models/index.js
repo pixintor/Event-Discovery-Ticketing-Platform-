3d844ded55ae43e76ad4415f3962cb6d3aaa2c33
pixintor <chrismayor4@yahoo.com>
2026-08-07 18:41:34 +0100

    user model refactor

--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,8 @@ import sequelize from "../config/database.js";
 
 import User from "./user.js";
 import Event from "./event.js";
-import Registration from "./registration.js";
-import Category from "./category.js";
+import Registration from "./Registration.js";
+import Category from "./Category.js";
 import TicketType from "./TicketType.js";
 import CampaignModel from "./Campaign.js";
 
